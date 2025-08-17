1133b4bd335bc0fe85dfcfd54c7da7e90c3e3744
brarodriguezfl <brarodriguezfl@gmail.com>
2025-08-16 21:15:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 # Convertir a lista para el multiselect
 fruit_names = my_dataframe.to_pandas()['FRUIT_NAME'].tolist()
